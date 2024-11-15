d341514ff58ab848e1ca1ad798d1d494a62e451c
dfafedd <jbbonge@gmail.com>
2024-11-15 12:58:42 +0900

    Delete all files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "normalize.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
